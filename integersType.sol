06dab00843f7c463f0cff919d426ca416c4a4fe8
Mahmoud Mourad <mahmoud.mourad.dev@gmail.com>
2025-05-04 08:45:38 +0100

    Add files via upload

---
+++ integersType.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.29;
+ contract typeIntegers{
+    // sign negative and positive value 
+    int8 a= -1;
+    int16 b = 1;
+    int32 c= 2 ;
+    int64 d =  3  ;
+    int128 e = 4;
+    int256 f= 5;
+    int g = -50;  // like int256
+
+    // unsign only positive value
+    uint8 A = 0;
+    uint16 B=1 ;
+    uint32 C=2;
+    uint64 D=3;
+    uint128 E=4;
+    uint256 F= 5;
+    uint G =  50 ;  // like  uint256
+   
+     
+
+ }
